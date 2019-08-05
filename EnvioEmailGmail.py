296d55805d1e91eb0876f6727a3670b28d3f7aea
FerPoletto <fernandompoletto@gmail.com>
2019-08-05 19:22:51 -0300

    Email

--- EnvioEmailGmail.py
+++ EnvioEmailGmail.py
@@ -23,6 +23,6 @@ try:
     server.sendmail(sent_from, to, email_text)
     server.close()
 
-    print 'Email sent!'
+    print('Email sent!')
 except:
-    print 'Something went wrong...'
+    print('Something went wrong...')
