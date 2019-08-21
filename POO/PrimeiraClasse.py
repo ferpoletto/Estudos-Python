35beae83f99a3e6cc515e93d1a95249f4e591619
FerPoletto <fernandompoletto@gmail.com>
2019-08-20 21:43:17 -0300

    PrimeiraClasse

--- POO/PrimeiraClasse.py
+++ POO/PrimeiraClasse.py
@@ -7,9 +7,18 @@ class retangulo:
     def area(self):
         return self.c * self.l
 
+
 teste = retangulo()
 
 teste.l = 2
+
 teste.c = 2
 
 print(teste.area())
+
+teste2 = retangulo()
+
+teste2.l = 4
+teste2.c = 60
+
+print(teste2.area())
