4a6be357a7b7e9564858cdca8d776ed1be1d2e1f
Fernando Poletto <fernandompoletto@gmail.com>
2019-08-21 19:07:08 -0300

    Validação

--- Diversos/Funcoes Data/conversor.py
+++ Diversos/Funcoes Data/conversor.py
@@ -3,7 +3,7 @@ def converte_data_nascimento(data_nascimento):
     #Tupla que contem o nome dos meses por extenso. Tupla é um tipo de lista que tem como caracteristica princial ser imutável.
     meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho','Agosto', 'Setembro', 'Outubro', 'Novembro','Dezembro')
 
-    # Utilizando o método SPLIT do Python, a string será dividida a cada '/', e adicionada nas variáveis respectivamente
+    # Utilizando o método SPLIT do Python a string será dividida a cada '/', e adicionada nas variáveis dia, mes ano, respectivamente
     dia, mes, ano = data_nascimento.split('/')
 
     #Imprimir na tela a data formatada, buscando na Tupla meses o mes informado pelo usuario
