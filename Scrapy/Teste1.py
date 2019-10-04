ebdf18969473f93a6a6f6a19eede80f8848066cb
Fernando Poletto <fernandompoletto@gmail.com>
2019-10-04 11:03:49 -0300

    Retirando Scrapy

--- Scrapy/Teste1.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class MainOLX(scrapy.spider):
-    name = "TesteOLX"
-
-    start_urls = ['www.olx.com.br']
-
-    def parse(self, response):
-        self.log(f"Estou em: {response.url}")
